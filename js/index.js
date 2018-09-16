f1533a34011871a9334109ece53b6f31ff7d3d32
Julie Jonak <juliejonak10@gmail.com>
2018-09-16 10:02:42 -0500

    Added image randomizer JS to HTML via script tag

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@
 // timerStart.addEventListener('click', ()=>{setAlarm()});
 
 
-
 let button = document.querySelector('.go-button');
 button.addEventListener('click', ()=>{
     // setAlarm();
@@ -15,31 +14,6 @@ button.addEventListener('click', ()=>{
     button.style = 'width: 72%; background-color: #228B22; color: white; font-size: 16px; font-weight: 500; letter-spacing: 2.6px;';
 });
 
-
-
-//Randomize the image shown on load
-
-// class Image {
-//     constructor(element) {
-//         this.element = element;
-
-//         this.images = document.querySelectorAll('.random');
-
-//         addEventListener('load',)
-//     }
-// }
-
-// let images = document.querySelectAll('.random');
-// images = Array.from(images).map((item)=>{new Image(item)})
-
-
-// function randomImage() {
-
-//     var i = Math.floor(Math.random()*images.length);
-  
-//     // document.getElementById(“random”).src=”Raw Images/” + images[i];
-  
-//   }
 
 
 
