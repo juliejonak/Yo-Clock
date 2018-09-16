bdcbc085183c54129c827eb6a44c1c4198ccc719
Julie Jonak <juliejonak10@gmail.com>
2018-09-16 08:52:44 -0500

    Adjusted successful button inner HTML and button design on click

--- js/index.js
+++ js/index.js
@@ -2,10 +2,18 @@
 
 //On button click, sets user's timer
 
-let timerStart = document.querySelector('.button');
+// let timerStart = document.querySelector('.go-button');
 
-timerStart.addEventListener('click', ()=>{setAlarm()});
+// timerStart.addEventListener('click', ()=>{setAlarm()});
 
+
+
+let button = document.querySelector('.go-button');
+button.addEventListener('click', ()=>{
+    // setAlarm();
+    button.innerHTML = 'Timer Started!';
+    button.style = 'width: 72%; background-color: #228B22; color: white; font-size: 16px; font-weight: 500; letter-spacing: 2.6px;';
+});
 
 
 
